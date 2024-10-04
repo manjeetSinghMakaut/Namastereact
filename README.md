caa695af706f6552f62313a0d1423a549bc386db
Manjeet Singh <manjeetsingh.makaut@gmail.com>
2024-10-05 04:33:13 +0530

    second commit about the initials

---
+++ README.md
@@ -0,0 +1,19 @@
+# parcel
+- dev build
+- local server
+- HMR = hot module replacement
+- file watching algorithm - written in c++
+- faster build because of caching
+- image optimization
+- minfiacation of files
+- bundling
+- compress
+- consistent hashing
+- code splitting
+- differential bundling - to support  older browser
+- error handling
+- https
+
+
+  
+
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
 const parent =React.createElement(
     "div",
     {id : "parent"},
     [React.createElement(
         "div",
         {id : "child"} ,
-        [React.createElement('h1',{}, "i am an h1 tag"),React.createElement('h2',{}, "i am an h2  tag")]
+        [React.createElement('h1',{}, "this is namaste react"),React.createElement('h2',{}, "i am an h2  tag")]
     ) ,React.createElement(
         "div",
         {id : "child2"} ,
@@ -15,12 +18,9 @@ const parent =React.createElement(
 
 
 
-
-
-
-// const header = React.createElement("h1",{id: "header"},"hello world")
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root =ReactDom.createRoot(document.getElementById("root"));
 
 root.render(parent);
-console.log(parent)
 
+
+ 
