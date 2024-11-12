298106b8e1bae51ef6e788585d22d80b04368639
Manjeet Singh <manjeetsingh.makaut@gmail.com>
2024-11-13 02:58:30 +0530

    installed Tailwind css

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -18,7 +18,7 @@ const RestaurantMenu = () => {
   } = resInfo?.cards[2]?.card?.card?.info;
 
   const { itemCards } =
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card.card;
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card.card;
 
   console.log(itemCards);
 
